c119136428123ad9f199ff0a2a3ebed49e950ce8
jonakh2002 <jonakh2002@gmail.com>
2025-11-27 10:37:11 +0100

    Destillat klasser

--- src/model/FærdigVare.java
+++ src/model/FærdigVare.java
@@ -1,4 +1,15 @@
 package model;
 
 public class FærdigVare {
+    private String navn;
+    private int pris;
+    private int mængde;
+
+    //Link til destillat, 1 (Skal med i constructor)
+
+    public FærdigVare(String navn, int pris, int mængde) {
+        this.navn = navn;
+        this.pris = pris;
+        this.mængde = mængde;
+    }
 }
