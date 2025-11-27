2f741bfcb70216fe7c313fbfced856e1b5db9b5d
krillebc-school <eaa25cbc@students.eaaa.dk>
2025-11-27 10:35:11 +0100

    Destillering, Medarbejder, Oprindelse, Råvare attributter tilføjet

--- src/model/Oprindelse.java
+++ src/model/Oprindelse.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Oprindelse {
+
+    private String mark;
+    private String gaard;
+
+    public Oprindelse(String mark, String gaard) {
+        this.mark = mark;
+        this.gaard = gaard;
+    }
 }
