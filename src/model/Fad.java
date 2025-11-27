0f9990eaac0dc7dc04ca87f11310ffa7e606f6f8
Gustav <eaa25glb@students.eaaa.dk>
2025-11-27 11:08:34 +0100

    Fad, leverandør linkatributter

--- src/model/Fad.java
+++ src/model/Fad.java
@@ -6,16 +6,29 @@ public class Fad {
     private boolean erAktiv;
     private Destillat destillat;
     private FadType fadType;
-    private Leverandør leverandør;
     private Hylde hylde;
 
-    public Fad(int id, double størrelse, boolean erAktiv, FadType fadType,
-               Leverandør leverandør, Hylde hylde) {
+    public Fad(int id, double størrelse, boolean erAktiv, FadType fadType, Hylde hylde) {
         this.id = id;
         this.størrelse = størrelse;
         this.erAktiv = erAktiv;
         this.fadType = fadType;
-        this.leverandør = leverandør;
+        this.hylde = hylde;
+    }
+
+    public Destillat getDestillat(){
+        return destillat;
+    }
+
+    public void setDestillat(Destillat destillat) {
+        this.destillat = destillat;
+    }
+
+    public Hylde getHylde() {
+        return hylde;
+    }
+
+    public void setHylde(Hylde hylde) {
         this.hylde = hylde;
     }
 }
