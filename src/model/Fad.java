9e973d25d4a5f2f8f5f27274684fd5c4583f4d65
Gustav <eaa25glb@students.eaaa.dk>
2025-11-27 10:42:03 +0100

    Fad, fadtype, leverandør

--- src/model/Fad.java
+++ src/model/Fad.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Fad {
+    private int id;
+    private double størrelse;
+    private boolean erAktiv;
+    private Destillat destillat;
+    private FadType fadType;
+    private Leverandør leverandør;
+    private Hylde hylde;
+
+    public Fad(int id, double størrelse, boolean erAktiv, FadType fadType,
+               Leverandør leverandør, Hylde hylde) {
+        this.id = id;
+        this.størrelse = størrelse;
+        this.erAktiv = erAktiv;
+        this.fadType = fadType;
+        this.leverandør = leverandør;
+        this.hylde = hylde;
+    }
 }
