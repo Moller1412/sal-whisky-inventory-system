25b3fb17021584e78e1772164c11a7c3fd31ef23
jonakh2002 <jonakh2002@gmail.com>
2025-12-03 09:38:23 +0100

    Leverandør klasse ændring

--- src/model/Leverandør.java
+++ src/model/Leverandør.java
@@ -4,21 +4,11 @@ public class Leverandør {
     private String navn;
     private String adresse;
     private int mobilnummer;
-    private Fad fad;
 
     public Leverandør(String navn, String adresse, int mobilnummer ) {
         this.navn = navn;
         this.adresse = adresse;
         this.mobilnummer = mobilnummer;
-
-    }
-
-    public Fad getFad() {
-        return fad;
-    }
-
-    public void setFad(Fad fad) {
-        this.fad = fad;
     }
 
     @Override
