723056cdaccb97a47344eaace4eef10ed09f0aa9
krillebc-school <eaa25cbc@students.eaaa.dk>
2025-12-08 14:44:49 +0100

    mere arbejde på LagerTab

--- src/gui/LagerTab.java
+++ src/gui/LagerTab.java
@@ -1,7 +1,6 @@
 package gui;
 
 import Controller.Controller;
-import Storage.Storage;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -24,11 +23,14 @@ public class LagerTab implements Updatable{
     private ListView<Hylde> hyldeListView = new ListView<>();
     Button opretHylde = new Button("Opret Hylde");
 
-    private TextField medNavn = new TextField();
-    private TextField medNr = new TextField();
-    private TextField medTLF = new TextField();
+    //lager popup
+    private Stage lagerPopup;
+    private TextField lagerNavn = new TextField();
+    private TextField antalKVM = new TextField();
 
-    private Stage popup;
+    // Reol popup
+    private Stage reolPopup;
+    private TextField RoelNr = new TextField();
 
     public GridPane getContent() {
         GridPane pane = new GridPane();
@@ -45,6 +47,14 @@ public class LagerTab implements Updatable{
         LagerListView.getItems().setAll(Controller.getLagere());
         pane.add(LagerListView, 0,1,1,1);
 
+        ReolListView.getSelectionModel().selectedItemProperty().addListener((obs, oldItem, newItem) -> {
+            if (newItem != null) {
+                ReolListView
+                historieTxtArea.appendText(Controller.printHistorie(færdigvareListView.getSelectionModel().getSelectedItem()));
+
+            }
+        });
+
         pane.add(new Label("Reol"), 1,0);
         ReolListView.getItems().setAll(Controller.getReoler());
         pane.add(ReolListView,1,1,1,1);
@@ -54,13 +64,14 @@ public class LagerTab implements Updatable{
         pane.add(rækkeListView,2,1,1,1);
 
         pane.add(new Label("Række"), 3,0);
+        hyldeListView.getItems().setAll(Controller.getHylder());
         pane.add(hyldeListView,3,1,1,1);
 
         pane.add(opretLagerBtn,0,2);
-        opretLagerBtn.setOnAction(event -> opretLager());
+        opretLagerBtn.setOnAction(event -> opretLagerPopUp());
 
         pane.add(opretReolBtn,1,2);
-        opretReolBtn.setOnAction(event -> opretReol());
+        opretReolBtn.setOnAction(event -> setOpretReolPopup());
 
         pane.add(opretRækkeBtn,2,2);
         opretRækkeBtn.setOnAction(event -> opretRække());
@@ -68,11 +79,56 @@ public class LagerTab implements Updatable{
         pane.add(opretHylde,3,2);
         opretHylde.setOnAction(event -> opretHylde());
 
-
         return pane;
     }
 
+    private void opretLagerPopUp() {
+
+        lagerPopup = new Stage();
+        lagerPopup.setTitle("Opret lager");
+
+        Button knap = new Button("Opret lager");
+
+        VBox box = new VBox(10);
+        box.setPadding(new Insets(10));
+        box.getChildren().add(new Label("Navn: "));
+        box.getChildren().add(lagerNavn);
+        box.getChildren().add(new Label("Antal kvadratmeter: "));
+        box.getChildren().add(antalKVM);
+        box.getChildren().add(knap);
+
+        knap.setOnAction(event -> opretLager());
+
+        lagerPopup.setScene(new Scene(box, 200, 200));
+        lagerPopup.show();
+    }
+
     public void opretLager(){
+        Controller.createLager(lagerNavn.getText(),Integer.parseInt(antalKVM.getText()));
+        lagerNavn.clear();
+        antalKVM.clear();
+        lagerPopup.close();
+        update();
+    }
+
+    public void setOpretReolPopup(){
+
+        if (LagerListView.getSelectionModel().getSelectedItem() == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Vælg venligst et lager du vil oprette en reol til");
+            alert.show();
+        } else {
+            update();
+            reolPopup = new Stage();
+            reolPopup.setTitle("Opret reol");
+
+            VBox box = new VBox(10);
+            box.setPadding(new Insets(10));
+            box.getChildren().add(new Label("NR"));
+            box.getChildren().add(RoelNr);
+
+            reolPopup.setScene(new Scene(box, 200, 200));
+            reolPopup.show();
+        }
 
     }
 
@@ -90,7 +146,10 @@ public class LagerTab implements Updatable{
 
     @Override
     public void update() {
-
+        LagerListView.getItems().setAll(Controller.getLagere());
+        ReolListView.getItems().setAll(Controller.getReoler());
+        rækkeListView.getItems().setAll(Controller.getRækker());
+        hyldeListView.getItems().setAll(Controller.getHylder());
     }
 
 }
